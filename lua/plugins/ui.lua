4b3b4c703492845867aab2d5350da630c3122df3
Bin Hu <elithrade@gmail.com>
2025-08-16 17:05:36 +0800

    move coding.lua

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,20 +1,5 @@
 return {
   {
-    "hrsh7th/nvim-cmp",
-    opts = {
-      formatting = {
-        format = function(_, item)
-          item.menu = ""
-          local icons = require("lazyvim.config").icons.kinds
-          if icons[item.kind] then
-            item.kind = icons[item.kind] .. item.kind
-          end
-          return item
-        end,
-      },
-    },
-  },
-  {
     "folke/snacks.nvim",
     opts = {
       notifier = {
