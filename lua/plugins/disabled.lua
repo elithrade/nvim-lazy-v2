6f317a8def1230176f9b641140e6a94359478ae1
Bin Hu <elithrade@gmail.com>
2025-08-16 22:51:26 +0800

    merge telescope configs into telecope.lua

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -3,12 +3,4 @@ return {
     "folke/flash.nvim",
     enabled = false,
   },
-  {
-    "folke/snacks.nvim",
-    keys = {
-      -- Disable keys for snacks file explorer
-      { "<leader>e", false },
-      { "<leader>E", false },
-    },
-  },
 }
