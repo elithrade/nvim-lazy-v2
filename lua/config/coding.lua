13e04764c75fe5fcc32cb7d3b3abf3560bdb2930
Bin Hu <elithrade@gmail.com>
2025-08-16 11:19:10 +0800

    remove rust-analyzer from lsp config

--- lua/config/coding.lua
+++ lua/config/coding.lua
@@ -2,11 +2,6 @@ return {
   {
     "neovim/nvim-lspconfig",
     opts = {
-      setup = {
-        rust_analyzer = function()
-          return true
-        end,
-      },
       diagnostics = {
         virtual_text = false,
       },
