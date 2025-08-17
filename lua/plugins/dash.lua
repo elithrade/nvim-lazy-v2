769f2c36d43d40c926775ea8e8cf6cff45f2e2ff
Bin Hu <elithrade@gmail.com>
2025-08-17 22:21:34 +0800

    sudo apt install fortune cowsay lolcat

--- lua/plugins/dash.lua
+++ lua/plugins/dash.lua
@@ -3,17 +3,24 @@ return {
   opts = {
     dashboard = {
       preset = {
-        header = [[
-                                                    o                    
-                                                  _<|>_                  
-                                                                         
- \o__ __o     o__  __o     o__ __o     o      o     o    \o__ __o__ __o  
-  |     |>   /v      |>   /v     v\   <|>    <|>   <|>    |     |     |> 
- / \   / \  />      //   />       <\  < >    < >   / \   / \   / \   / \ 
- \o/   \o/  \o    o/     \         /   \o    o/    \o/   \o/   \o/   \o/ 
-  |     |    v\  /v __o   o       o     v\  /v      |     |     |     |  
- / \   / \    <\/> __/>   <\__ __/>      <\/>      / \   / \   / \   / \ 
-        ]],
+        keys = {
+          { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+          { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+          { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+          { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
+          { icon = " ", key = "x", desc = "Lazy Extras", action = ":LazyExtras" },
+          { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+        },
+      },
+      sections = {
+        {
+          section = "terminal",
+          cmd = "fortune -s -n 80 | cowsay | lolcat",
+          padding = 1,
+          indent = 8,
+        },
+        { section = "keys", gap = 1, padding = 1 },
+        { section = "startup" },
       },
     },
   },
