c3066eb391ef37b6908803b2236bb73aed370fd1
Bin Hu <elithrade@gmail.com>
2025-08-17 20:55:05 +0800

    add dashboard config

---
+++ lua/plugins/dash.lua
@@ -0,0 +1,18 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    dashboard = {
+      preset = {
+        header = [[
+________ooo____oo___oo____oo___________________________oo_________
+_ooooo___oo_________oo____oo_ooo__oo_ooo___ooooo___oooooo__ooooo__
+oo____o__oo____oo__oooo___ooo___o_ooo___o_oo___oo_oo___oo_oo____o_
+ooooooo__oo____oo___oo____oo____o_oo______oo___oo_oo___oo_ooooooo_
+oo_______oo____oo___oo__o_oo____o_oo______oo___oo_oo___oo_oo______
+_ooooo__ooooo_oooo___ooo__oo____o_oo_______oooo_o__oooooo__ooooo__
+__________________________________________________________________
+        ]],
+      },
+    },
+  },
+}
