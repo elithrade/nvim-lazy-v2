032d9add2d1e5ba5a0d2e23d9dea027d8323ae29
Bin Hu <elithrade@gmail.com>
2025-08-17 19:09:22 +0800

    take the first letter of mode

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -7,6 +7,17 @@ return {
         component_separators = "|",
         section_separators = "",
       },
+      sections = {
+        lualine_a = {
+          {
+            "mode",
+            -- Just take first character of mode
+            fmt = function(str)
+              return str:sub(1, 1)
+            end,
+          },
+        },
+      },
     },
   },
 }
