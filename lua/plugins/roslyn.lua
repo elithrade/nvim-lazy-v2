9d9e4e22f556bbdcce355a755e2e25452991c440
Bin Hu <elithrade@gmail.com>
2025-08-18 21:30:58 +0800

    remove workaround for omnisharp

---
+++ lua/plugins/roslyn.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "seblyng/roslyn.nvim",
+    ---@module 'roslyn.config'
+    ---@type RoslynNvimConfig
+    opts = {
+      -- your configuration comes here; leave empty for default settings
+    },
+  },
+  {
+    "Issafalcon/neotest-dotnet",
+  },
+}
