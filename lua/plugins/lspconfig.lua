14d6146d2531f1c7beddd2ac56a108ee56f5aeec
Bin Hu <elithrade@gmail.com>
2026-02-15 12:00:23 +0800

    attempt to address ts server and eslint hanging issue - disable tsserver and vtsls

---
+++ lua/plugins/lspconfig.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        tsserver = false, -- Disable tsserver as typescript-tools.nvim is used
+        vtsls = false, -- Disable vtsls as typescript-tools.nvim is used
+      },
+    },
+  },
+}
--- lua/plugins/typescript.lua
+++ lua/plugins/typescript.lua
@@ -1,45 +1,5 @@
 return {
-  {
-    "neovim/nvim-lspconfig",
-    ---@class LspInitOptions
-    opts = function(plugin, opts)
-      local util = require("lspconfig.util")
-
-      -- define your custom servers
-      local my_servers = {
-        vtsls = {
-          root_dir = util.root_pattern("tsconfig.json", "jsconfig.json", "package.json", ".git"),
-          init_options = {
-            maxTsServerMemory = 4096,
-          },
-          on_attach = function(client, bufnr)
-            -- We need to make sure the results of this are handled asynchronously
-            -- so that we don't block the UI
-            vim.keymap.set("n", "<leader>co", function()
-              vim.lsp.buf.rename()
-            end, { buffer = bufnr, desc = "Rename" })
-          end,
-        },
-        eslint = {
-          root_dir = util.root_pattern("eslint.config.js", ".eslintrc.json", ".eslintrc.js", "package.json", ".git"),
-          init_options = {
-            -- Add any eslint-specific init options here
-          },
-          on_attach = function(client, bufnr)
-            -- We need to make sure the results of this are handled asynchronously
-            -- so that we don't block the UI
-            vim.api.nvim_create_autocmd("BufWritePre", {
-              buffer = bufnr,
-              callback = function()
-                vim.lsp.buf.format({ bufnr = bufnr, async = true })
-              end,
-            })
-          end,
-        },
-      }
-
-      -- merge the servers returned by the function with the default servers
-      vim.tbl_deep_extend("force", opts.servers, my_servers)
-    end,
-  },
+  "pmizio/typescript-tools.nvim",
+  dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+  opts = {},
 }
